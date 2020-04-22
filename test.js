3ee42f3ae20e26fb39e478e6d4db9c062fef6b66
michallos1 <42137480+michallos1@users.noreply.github.com>
2020-04-22 15:15:54 +0200

    Update test.js

--- test.js
+++ test.js
@@ -5,4 +5,28 @@ class Klasa {
         this.Str = 0;
         this.Gold = 0;
     }
+<<<<<<< Updated upstream
+=======
+
+     LevelUp(Int,Dex,Str) { 
+    }
+
+}
+
+
+class Wojownik extends Klasa {
+    constructor(){
+        this.Hp = 0;
+        this.Def = 0;
+        this.Mana = 0;
+        this.Attack = 0;
+        this.Ekwipunek = Eq;
+        this.Level = 0;
+        this.Backpack = new Array[12];
+    }
+
+    LevelUp(){
+        
+    }
+>>>>>>> Stashed changes
 }
