96028e1ee4d2e56bfde85f59116dd2e9a6f5dc23
michallos1 <42137480+michallos1@users.noreply.github.com>
2020-06-19 19:06:13 +0200

    zm

--- Projekt/script/potwor.js
+++ Projekt/script/potwor.js
@@ -1,31 +1,28 @@
 class potwor {
-	constructor(){
-		this.Hp=10;
-		this.Atk=1; 
-		this.Def=1; 
-	}
-}
+    constructor() {
+        this.Hp = 10;
+        this.Atk = 1;
+        this.Def = 1;
+    }
 
-walka (Klasa){
-	potwor p;
-	p = ustaw(Klasa.lvl);
-	
-	do{
-		p.Hp-=(Klasa.Attack-p.Def);
-		Klasa.Hp-=(p.Atk-Klasa.Def);
-	}while(Klasa.Hp>0 && p.Hp>0);
-	
-	if(p.Hp<1)
-		return 1; //wygrana walka 
-	
-	return 0; // przegrana walka	
-}
+    ustaw(lvl) {
+        this.Hp += lvl * 5;
+        this.Atk += lvl;
+        this.Def += (1 / 3) * lvl;
+    }
 
-ustaw (lvl) {
-	potwor p
-	p.Hp+=lvl*5; 
-	p.Atk+=lvl;
-	p.Def+=(1/3)*lvl;
-	
-	return p;
+
+};
+
+function walka(Klasa) {
+    p = ustaw(Klasa.lvl);
+    do {
+        p.Hp -= (Klasa.Attack - p.Def);
+        Klasa.Hp -= (p.Atk - Klasa.Def);
+    } while (Klasa.Hp > 0 && p.Hp > 0);
+
+    if (p.Hp < 1)
+        return 1; //wygrana walka 
+
+    return 0; // przegrana walka	
 }
