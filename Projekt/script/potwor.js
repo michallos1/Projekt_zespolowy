ee33a5cb400e9242df2fd6e5ff4c284cdb7609e1
Tomek <Wlod.tomasz1@gmail.com>
2020-06-19 20:01:07 +0200

    potwor

--- Projekt/script/potwor.js
+++ Projekt/script/potwor.js
@@ -2,7 +2,7 @@ class potwor {
     constructor() {
         this.Hp = 10;
         this.Atk = 1;
-        this.Def = 1;
+        this.Def = 0;
     }
 
     ustaw(lvl) {
@@ -17,14 +17,14 @@ class potwor {
     walka(Klasa) {
         do {
             this.Hp -= (Klasa.Attack - this.Def);
-            console.log('Hp potwora: ' + this.hp);
+            console.log('Hp potwora: ' + this.Hp);
 
             if (this.Hp < 1) {
                 return 1; //wygrana walka 
             }
 
             Klasa.Hp -= (this.Atk - Klasa.Def);
-            console.log("Hp postaci: " + Klasa.hp);
+            console.log("Hp postaci: " + Klasa.Hp);
         } while (Klasa.Hp > 0);
 
         return 0; // przegrana walka	
